84ded44cb385626894da25f198f0253f86c4b12b
tifuseini <tifuseini@st.ug.edu.gh>
2022-11-27 20:16:16 +0000

    Uodate

---
+++ src/main/java/com/tutorial/springRest/model/Option.java
@@ -0,0 +1,15 @@
+package com.tutorial.springRest.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+public class Option {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "option_id")
+    private Long id;
+
+    @Column(name = "option_name")
+}
