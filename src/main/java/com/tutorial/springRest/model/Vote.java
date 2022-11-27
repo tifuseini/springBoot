d019016b004f7bea25492dd37f181c1759e0edfb
tifuseini <tifuseini@st.ug.edu.gh>
2022-11-27 20:57:31 +0000

    Option Repo

--- src/main/java/com/tutorial/springRest/model/Vote.java
+++ src/main/java/com/tutorial/springRest/model/Vote.java
@@ -1,4 +1,18 @@
 package com.tutorial.springRest.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
 public class Vote {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "vote_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "option_id")
+    private Option option;
 }
--- src/main/java/com/tutorial/springRest/repository/OptionRepository.java
+++ src/main/java/com/tutorial/springRest/repository/OptionRepository.java
@@ -1,2 +1,8 @@
-package com.tutorial.springRest.repository;public class OptionRepository {
+package com.tutorial.springRest.repository;
+
+import com.tutorial.springRest.model.Option;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OptionRepository extends JpaRepository<Option, Long> {
 }
+
