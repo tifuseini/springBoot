7cb60b7b300ced119f1ccc7280e01d38ae776f05
tifuseini <tifuseini@st.ug.edu.gh>
2022-11-27 20:33:21 +0000

    Update to poll abd addition of vote class

--- src/main/java/com/tutorial/springRest/model/Poll.java
+++ src/main/java/com/tutorial/springRest/model/Poll.java
@@ -1,4 +1,24 @@
 package com.tutorial.springRest.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Set;
+
+@Entity
+@Data
 public class Poll {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "poll_id")
+    private Long id;
+
+    @Column(name = "poll_question")
+    private String question;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "poll_id")
+    @OrderBy
+    private Set<Option> options;
 }
