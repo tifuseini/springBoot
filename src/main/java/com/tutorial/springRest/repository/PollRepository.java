2b0cf87ac81071ddf51670de509acc1fcc34576d
tifuseini <tifuseini@st.ug.edu.gh>
2022-11-27 21:01:28 +0000

    Update to repositpory

--- src/main/java/com/tutorial/springRest/repository/PollRepository.java
+++ src/main/java/com/tutorial/springRest/repository/PollRepository.java
@@ -1,4 +1,8 @@
 package com.tutorial.springRest.repository;
 
-public interface PollRepository {
+import com.tutorial.springRest.model.Poll;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PollRepository extends JpaRepository<Poll, Long> {
 }
+
--- src/main/java/com/tutorial/springRest/repository/VoteRepository.java
+++ src/main/java/com/tutorial/springRest/repository/VoteRepository.java
@@ -1,2 +1,8 @@
-package com.tutorial.springRest.repository;public class VoteRepository {
+package com.tutorial.springRest.repository;
+
+import com.tutorial.springRest.model.Vote;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VoteRepository extends JpaRepository<Vote, Long> {
 }
+
